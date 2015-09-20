9f3d8a351b606b53a011094db3b6b9cab8aaaa6c
Logan Allen <logan.allen@sbcglobal.net>
2015-09-19 21:53:32 -0400

    Changes

---
+++ script.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+    $("#me").click(function(){
+      if ( $( ".myDiv" ).is( ":hidden" ) ) {
+        $( ".myDiv" ).slideDown( "slow" );
+      } else {
+        $( ".myDiv" ).hide();
+      }
+    });
+
+    $("#you").
+      if ( $( ".myDiv" ).is( ":hidden" ) ) {
+        $( ".myDiv" ).slideDown( "slow" );
+      } else {
+        $( ".myDiv" ).hide();
+      }
+    });
+
+});
