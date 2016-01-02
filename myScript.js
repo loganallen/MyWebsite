9dae434e99e89d6e822eeb6b51314d2b8f5fee6c
Logan Allen <logan.allen@sbcglobal.net>
2016-01-01 18:08:15 -0800

    Added project tiles with hover transitions

--- myScript.js
+++ myScript.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
     $('[rel="tooltip"]').tooltip(); 
 });
 
-// Hide Header on on scroll down
+/* Hide Header on on scroll down */
 var didScroll;
 var lastScrollTop = 0;
 var delta = 10;
@@ -11,50 +11,79 @@ var navbarHeight = $('nav').outerHeight();
 
 // Make variable didScroll true if user scrolls
 $(window).scroll(function(event){
-    didScroll = true;
+  didScroll = true;
 });
 
 // Check for scrolling every 250ms
 setInterval(function() {
-    if (didScroll) {
-        hasScrolled();
-        didScroll = false;
-    }
+  if (didScroll) {
+    hasScrolled();
+    didScroll = false;
+  }
 }, 250);
 
 // Display navBar if scroll up, hide it if scroll down
 function hasScrolled() {
-    var st = $(this).scrollTop();
-    
-    // Make sure they scroll more than delta
-    if(Math.abs(lastScrollTop - st) <= delta)
-      return;
-    
-    // If they scrolled down and are past the navbar, add class .nav-up.
-    // This is necessary so you never see what is "behind" the navbar.
-    if (st > lastScrollTop && st > navbarHeight){
-        // Scroll Down
-        $('nav').removeClass('nav-down').addClass('nav-up');
-    } else {
-        // Scroll Up
-        if(st + $(window).height() < $(document).height()) {
-            $('nav').removeClass('nav-up').addClass('nav-down');
-        }
+  var st = $(this).scrollTop();
+  
+  // Make sure they scroll more than delta
+  if(Math.abs(lastScrollTop - st) <= delta)
+    return;
+  
+  // If they scrolled down and are past the navbar, add class .nav-up.
+  // This is necessary so you never see what is "behind" the navbar.
+  if (st > lastScrollTop && st > navbarHeight){
+    // Scroll Down
+    $('nav').removeClass('nav-down').addClass('nav-up');
+  } else {
+    // Scroll Up
+    if(st + $(window).height() < $(document).height()) {
+      $('nav').removeClass('nav-up').addClass('nav-down');
     }
-    
-    lastScrollTop = st;
+  }
+  
+  lastScrollTop = st;
 }
 
-// var main = function(){
-// 	$('#myFooter p').click(function(){
-//      //$('<div id="happiness">What the flock</div>').appendTo('#myFooter');
-//   });
-// };
+window.addEventListener("resize", hasResized);
+var screenWidth;
 
-// $(document).ready(main);
+function hasResized(){
+  screenWidth = $(window).width();
+}
 
-$(function(){
-    $('div.blocks div').inview({
-        'viewFactor': 0.3
-    });
+// Smooth scrolling when navbar links are clicked
+$(document).ready(function(){
+  $('#myNavbar ul li a').click(function(){
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+  $('#name').click(function(){
+    $('html, body').animate({
+      scrollTop: $( $.attr(this, 'href') ).offset().top
+    }, 500);
+    return false;
+  });
+});
+
+// $(function(){
+//     $('div.blocks div').inview({
+//         'viewFactor': 0.3
+//     });
+// });
+
+// $(function(){
+//   $('.stack').stack();
+// });
+
+// Caption Hover for project images
+$(function() {
+  $('.projectTile').captionHover({
+    fx: 'milo',
+    headColor: '#fff',
+    captionColor: '#fff',
+    overlay: '#2e5d5a', 
+  });
 });
