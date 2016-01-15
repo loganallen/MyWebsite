fe81e2abdd76600d971dd51e9ee4bfa6c9eca8d6
Logan Allen <logan.allen@sbcglobal.net>
2016-01-14 23:09:22 -0800

    Transition changes

--- myScript.js
+++ myScript.js
@@ -114,7 +114,7 @@ $(function() {
 });
 
 $(function(){
-    $('.projectTileContainer div').inview({'viewFactor': 0.6});
+    //$('.projectTileContainer div').inview({'viewFactor': 0.6});
     $('.blocks div').inview({'viewFactor': 0.6});
     $('#links').inview({'viewFactor': 0.5});
     $('.linkTile div').inview({'viewFactor': 1});
