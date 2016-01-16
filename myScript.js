fc09e6c99fee216c6f320044539dace6257f89ca
Logan Allen <logan.allen@sbcglobal.net>
2016-01-15 16:25:42 -0800

    Footer transition removal

--- myScript.js
+++ myScript.js
@@ -119,5 +119,5 @@ $(function(){
     $('.blocks div').inview({'viewFactor': 0.6});
     $('#links').inview({'viewFactor': 0.5});
     $('.linkTile div').inview({'viewFactor': 1});
-    $('#myFooter div').inview({'viewFactor': 0.1});
+    // $('#myFooter div').inview({'viewFactor': 0.1});
 });
