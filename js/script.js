e3ca7b9749553d24eb9b7c558c3d88cc6af767cb
Logan Allen <logan.allen@sbcglobal.net>
2016-05-31 23:12:49 -0700

    Temporary fix to about block spacing

--- js/script.js
+++ js/script.js
@@ -46,8 +46,8 @@ $(document).ready(function() {
     goToProjectPage(project_id);
   });
 
-  var bh = $('#education_wrapper').height();
-  $('.about_wrapper').css({'height':bh+'px'});
+  // var bh = $('#education_wrapper').height();
+  // $('.about_wrapper').css({'height':bh+'px'});
 
 });
 
